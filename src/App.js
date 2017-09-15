1155b4893472825cf0de38599decdfe8a68290bd
IgorSapo <amsterdam1611@gmail.com>
2017-09-15 18:35:05 +0400

    Seantic UI setup

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import HomePage from './components/pages/HomePage';
 import LoginPage from './components/pages/LoginPage';
+import { Container } from 'semantic-ui-react';
 
 const App = () => (
-  <div>
+  <Container>
     <Route exact path="/" component={HomePage} />
     <Route exact path="/login" component={LoginPage} />
-  </div>
+  </Container>
 );
 
 export default App;
