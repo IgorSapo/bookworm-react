3b5c3e851b76fb32231d350db4ad58273832fe49
IgorS <amsterdam1611@gmail.com>
2017-09-20 20:29:02 +0400

    Add authorized routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Route } from 'react-router-dom';
+import { Container } from 'semantic-ui-react';
 import HomePage from './components/pages/HomePage';
 import LoginPage from './components/pages/LoginPage';
-import { Container } from 'semantic-ui-react';
+import DashboardPage from './components/pages/DashboardPage';
+import UserRoute from './components/routes/UserRoute.jsx';
+import GuestRoute from './components/routes/GuestRoute.jsx';
 
-const App = () => (
+const App = ({ location }) => (
   <Container>
-    <Route exact path="/" component={HomePage} />
-    <Route exact path="/login" component={LoginPage} />
+    <Route location={location} exact path="/" component={HomePage} />
+    <GuestRoute location={location} exact path="/login" component={LoginPage} />
+    <UserRoute
+      location={location}
+      exact
+      path="/dashboard"
+      component={DashboardPage}
+    />
   </Container>
 );
+
+App.propTypes = {
+  location: PropTypes.shape({
+    pathname: PropTypes.string.isRequired
+  }).isRequired
+};
 
 export default App;
